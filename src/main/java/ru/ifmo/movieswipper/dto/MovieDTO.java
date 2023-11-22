067711bfcfb78e1e54997c8f443a0f76094a3b9d
HackMemory <dragon.warri0r@yandex.com>
2023-11-23 02:36:45 +0300

    fix: remove person

--- src/main/java/ru/ifmo/movieswipper/dto/MovieDTO.java
+++ src/main/java/ru/ifmo/movieswipper/dto/MovieDTO.java
@@ -1,13 +1,10 @@
 package ru.ifmo.movieswipper.dto;
 
 import info.movito.themoviedbapi.model.Genre;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
-import ru.ifmo.movieswipper.model.Person;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -22,7 +19,5 @@ public class MovieDTO {
     private String synopsys;
     private float rate;
     private LocalDate releaseDate;
-    private Person director;
-    private List<Person> actors;
     private List<Genre> genres;
 }
